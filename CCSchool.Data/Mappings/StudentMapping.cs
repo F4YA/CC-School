d5ed232f325452e8fcb0f5c1375a0d097ca8390e
Osvaldo Carvalho <osvaldocarvalho220@gmail.com>
2023-03-02 17:35:57 -0300

    [FEAT] New class StudentMapping added
    Added class with functionalities to generate an entity table into data base.

---
+++ CCSchool.Data/Mappings/StudentMapping.cs
@@ -0,0 +1,37 @@
+﻿using CCSchool.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSchool.Data.Mappings
+{
+    internal class StudentMapping : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.ToTable(nameof(Student));
+
+            builder.HasKey(x => x.RA);
+
+            builder.Property(x => x.Nationality)
+                    .HasColumnType("NVARCHAR")
+                    .HasMaxLength(60)
+                    .IsRequired();
+
+            builder.Property(x => x.PicLink)
+                    .HasColumnType("NVARCHAR");
+
+            builder.Property(x => x.Cortesy)
+                    .HasColumnType("BIT");
+
+            builder.HasOne<Person>()
+                    .WithOne()
+                    .HasForeignKey<Person>(x => x.PersonId)
+                    .IsRequired();
+        }
+    }
+}

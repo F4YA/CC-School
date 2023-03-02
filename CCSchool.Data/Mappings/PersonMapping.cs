3914fe82c9137701992163a3abaf52e13eb30486
Osvaldo Carvalho <osvaldocarvalho220@gmail.com>
2023-03-02 17:35:32 -0300

    [FEAT] New class PersonMapping added
    Added class with functionalities to generate an entity table into data base.

---
+++ CCSchool.Data/Mappings/PersonMapping.cs
@@ -0,0 +1,34 @@
+﻿using CCSchool.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CCSchool.Data.Mappings
+{
+    internal class PersonMapping : IEntityTypeConfiguration<Person>
+    {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.ToTable(nameof(Person));
+
+            builder.HasKey(c => c.PersonId);
+
+            builder.Property(c => c.Name)
+                    .HasColumnType("NVARCHAR")
+                    .HasMaxLength(60)
+                    .IsRequired();
+
+            builder.Property(c => c.Email)
+                    .HasColumnType("NVARCHAR")
+                    .HasMaxLength(60)
+                    .IsRequired();
+
+            builder.Property(c => c.Password)
+                    .HasColumnType("NVARCHAR")
+                    .IsRequired();
+
+            builder.Property(c => c.Birth)
+                    .HasColumnType("DATE")
+                    .IsRequired();
+        }
+    }
+}

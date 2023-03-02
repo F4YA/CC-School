351767aa226d8d95afd47885a07b1c7156a28d87
Osvaldo Carvalho <osvaldocarvalho220@gmail.com>
2023-03-02 13:49:52 -0300

    [FEAT] New class Phone added

---
+++ CCSchool.Business/Models/Phone.cs
@@ -0,0 +1,9 @@
+﻿namespace CCSchool.Business.Models
+{
+    public class Phone
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public int PersonId { get; set; }
+    }
+}

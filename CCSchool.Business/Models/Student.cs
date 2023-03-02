7398066ad2fabeae95fc608ba3162a6e75824dff
Osvaldo Carvalho <osvaldocarvalho220@gmail.com>
2023-03-02 10:55:21 -0300

    [FEAT] New class Student added

---
+++ CCSchool.Business/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSchool.Business.Models
+{
+    public class Student : Person
+    {
+        public int RA { get; set; }
+        public string Nationality { get; set; }
+        public string PicLink { get; set; }
+        public bool Cortesy { get; set; }
+    }
+}

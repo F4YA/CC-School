e8920a6acb1594efe6a39657702af962b2889f3a
Osvaldo Carvalho <osvaldocarvalho220@gmail.com>
2023-03-02 17:30:15 -0300

    [FEAT] New class Subject added

---
+++ CCSchool.Business/Models/Subject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSchool.Business.Models
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+
+        public List<Instructor> Instructors { get; set; }
+
+        public Subject()
+        {
+            Instructors = new List<Instructor>();
+        }
+    }
+}

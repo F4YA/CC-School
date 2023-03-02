3327a896e2e15e488bdd68193c1a4a9ea718a5cc
Osvaldo Carvalho <osvaldocarvalho220@gmail.com>
2023-03-02 17:37:50 -0300

    [FEAT] New class PhoneMapping added
    Added class with functionalities to generate an entity table into data base.

---
+++ CCSchool.Data/Mappings/PhoneMapping.cs
@@ -0,0 +1,25 @@
+﻿using CCSchool.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CCSchool.Data.Mappings
+{
+    internal class PhoneMapping : IEntityTypeConfiguration<Phone>
+    {
+        public void Configure(EntityTypeBuilder<Phone> builder)
+        {
+            builder.ToTable(nameof(Phone));
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Number)
+                    .HasColumnType("BIGINT")
+                    .IsRequired();
+
+            builder.HasOne<Person>()
+                    .WithMany(p => p.Phones)
+                    .HasForeignKey(x => x.PersonId)
+                    .IsRequired();
+        }
+    }
+}

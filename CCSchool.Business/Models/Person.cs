601785c524be2949fb6f5f29ee5d884a29a4bb28
Osvaldo Carvalho <osvaldocarvalho220@gmail.com>
2023-03-02 10:25:15 -0300

    [FEAT] New class Person added

---
+++ CCSchool.Business/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSchool.Business.Models
+{
+    public abstract class Person
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; private set; }
+        public int Phone { get; set; }
+        public DateOnly Birth { get; set; }
+
+        public List<Document> Documents { get; set; }
+    }
+}

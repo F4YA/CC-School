09e7523eaaa3f041a69f62cc8fb0777a724055e2
Osvaldo Carvalho <osvaldocarvalho220@gmail.com>
2023-03-02 17:36:36 -0300

    [FEAT] New class CoordinatorMapping added
    Added class with functionalities to generate an entity table into data base.

---
+++ CCSchool.Data/Mappings/CoordinatorMapping.cs
@@ -0,0 +1,31 @@
+﻿using CCSchool.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CCSchool.Data.Mappings
+{
+    internal class CoordinatorMapping : IEntityTypeConfiguration<Coordinator>
+    {
+        public void Configure(EntityTypeBuilder<Coordinator> builder)
+        {
+            builder.ToTable(nameof(Coordinator));
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.User)
+                    .HasColumnType("NVARCHAR")
+                    .HasMaxLength(30)
+                    .IsRequired();
+
+            builder.HasOne<Person>()
+                    .WithOne()
+                    .HasForeignKey<Person>(x => x.PersonId)
+                    .IsRequired();
+
+            builder.HasOne<Department>()
+                    .WithMany(d => d.Coordinators)
+                    .HasForeignKey(x => x.DepartmentId)
+                    .IsRequired();
+        }
+    }
+}

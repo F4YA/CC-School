aa37356347b709db508371481d6608f7acc05e18
Osvaldo Carvalho <osvaldocarvalho220@gmail.com>
2023-03-02 17:37:18 -0300

    [FEAT] New class DocumentMapping added
    Added class with functionalities to generate an entity table into data base.

---
+++ CCSchool.Data/Mappings/DocumentMapping.cs
@@ -0,0 +1,30 @@
+﻿using CCSchool.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CCSchool.Data.Mappings
+{
+    internal class DocumentMapping : IEntityTypeConfiguration<Document>
+    {
+        public void Configure(EntityTypeBuilder<Document> builder)
+        {
+            builder.ToTable(nameof(Document));
+
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Value)
+                    .HasColumnType("NVARCHAR")
+                    .HasMaxLength(20);
+
+            builder.HasOne<Person>()
+                    .WithMany(p => p.Documents)
+                    .HasForeignKey(e => e.PersonId)
+                    .IsRequired();
+
+            builder.HasOne<DocumentType>()
+                   .WithMany(dt => dt.Documents)
+                   .HasForeignKey(e => e.DocumentTypeId)
+                   .IsRequired();
+        }
+    }
+}

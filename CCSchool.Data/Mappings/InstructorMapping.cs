cb53334790272685ddff4cde06e6ad8153da6e3b
Osvaldo Carvalho <osvaldocarvalho220@gmail.com>
2023-03-02 17:36:11 -0300

    [FEAT] New class InstructorMapping added
    Added class with functionalities to generate an entity table into data base.

---
+++ CCSchool.Data/Mappings/InstructorMapping.cs
@@ -0,0 +1,32 @@
+﻿using CCSchool.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSchool.Data.Mappings
+{
+    internal class InstructorMapping : IEntityTypeConfiguration<Instructor>
+    {
+        public void Configure(EntityTypeBuilder<Instructor> builder)
+        {
+            builder.ToTable(nameof(Instructor));
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.User)
+                    .HasColumnType("NVARCHAR")
+                    .HasMaxLength(30)
+                    .IsRequired();
+
+            builder.Property(x => x.Active)
+                    .HasColumnType("BIT");
+
+            builder.HasMany(x => x.Subjects)
+                    .WithMany(s => s.Instructors);
+        }
+    }
+}

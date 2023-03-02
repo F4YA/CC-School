986cef9a33376fe11b7a842514cb74d3ffb098b7
Osvaldo Carvalho <osvaldocarvalho220@gmail.com>
2023-03-02 10:56:50 -0300

    [FEAT] New class Document added

---
+++ CCSchool.Business/Models/Document.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSchool.Business.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+        public int PersonId { get; set; }
+        public int DocumentTypeId { get; set; }
+
+        public DocumentType DocumentType { get; set; }
+    }
+}

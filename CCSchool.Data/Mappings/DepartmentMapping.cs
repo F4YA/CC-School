0bb38abcb0e14767b4852032fa99baf2271bd2ae
Osvaldo Carvalho <osvaldocarvalho220@gmail.com>
2023-03-02 17:37:04 -0300

    [FEAT] New class DepartmentMapping added
    Added class with functionalities to generate an entity table into data base.

---
+++ CCSchool.Data/Mappings/DepartmentMapping.cs
@@ -0,0 +1,21 @@
+﻿using CCSchool.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CCSchool.Data.Mappings
+{
+    internal class DepartmentMapping : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.ToTable(nameof(Department));
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                    .HasColumnType("NVARCHAR")
+                    .HasMaxLength(30);
+
+        }
+    }
+}

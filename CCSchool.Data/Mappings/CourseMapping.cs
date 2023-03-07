7296bf6461e9e410a3f01c86679e658be6d6df26
Osvaldo Carvalho <osvaldocarvalho220@gmail.com>
2023-03-06 22:42:12 -0300

    [FEAT] New class CourseMapping added
    Added class with functionalities to generate an entity table into data base.

---
+++ CCSchool.Data/Mappings/CourseMapping.cs
@@ -0,0 +1,33 @@
+﻿using CCSchool.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CCSchool.Data.Mappings
+{
+    internal class CourseMapping : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> builder)
+        {
+            builder.ToTable(nameof(Course));
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                    .HasColumnType("NVARCHAR")
+                    .HasMaxLength(60)
+                    .IsRequired();
+
+            builder.Property(x => x.Description)
+                    .HasColumnType("NVARCHAR")
+                    .HasMaxLength(500)
+                    .IsRequired();
+
+            builder.Property(x => x.Active)
+                    .HasColumnType("BYTE");
+
+            builder.Property(x => x.Type)
+                    .HasConversion<int>()
+                    .IsRequired();
+        }
+    }
+}
